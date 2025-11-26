a65917a9e59dc3a616c50831dcd32f46a0a0245d
João Vitor <jvitor.infoassistent1@gmail.com>
2025-11-26 14:56:57 -0300

    `Updated Expo project with new dependencies and styles for login page`

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { style } from './styles'
+
+export default function Login() {
+  return (
+    <View style={style.container}>
+      <View>
+        
+      </View>
+      <View></View>
+      <View></View>
+    </View>
+  )
+}
